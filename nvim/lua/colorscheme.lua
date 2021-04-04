2ff433d99c5b89c9a0317374a9e6efc19c51fa17
Joao Salles <joao.salles@check24.de>
2021-04-04 10:58:58 +0200

    Adjust lsp keymaps

--- nvim/lua/colorscheme.lua
+++ nvim/lua/colorscheme.lua
@@ -1,9 +1,3 @@
 vim.cmd('colorscheme onedark')
 vim.cmd('let g:nvcode_termcolors=256')
--- Errors in Red
-vim.cmd('hi LspDiagnosticsVirtualTextError guifg=Red ctermfg=Red')
--- Warnings in Yellow
-vim.cmd('hi LspDiagnosticsVirtualTextWarning guifg=Yellow ctermfg=Yellow')
--- Info and Hints in White
-vim.cmd('hi LspDiagnosticsVirtualTextInformation guifg=White ctermfg=White')
-vim.cmd('hi LspDiagnosticsVirtualTextHint guifg=White ctermfg=White')
+
--- nvim/lua/lsp/config.lua
+++ nvim/lua/lsp/config.lua
@@ -1,4 +1,17 @@
-vim.fn.sign_define("LspDiagnosticsSignError", { text = " 🞮", numhl = "LspDiagnosticsDefaultError" })
-vim.fn.sign_define("LspDiagnosticsSignWarning", { text = " ▲", numhl = "LspDiagnosticsDefaultWarning" })
-vim.fn.sign_define("LspDiagnosticsSignInformation", { text = " ⁈", numhl = "LspDiagnosticsDefaultInformation" })
-vim.fn.sign_define("LspDiagnosticsSignHint", { text = " ⯁", numhl = "LspDiagnosticsDefaultHint" })
+-- vim.fn.sign_define("LspDiagnosticsSignError", { text = " 🞮", numhl = "LspDiagnosticsDefaultError" })
+-- vim.fn.sign_define("LspDiagnosticsSignWarning", { text = " ▲", numhl = "LspDiagnosticsDefaultWarning" })
+-- vim.fn.sign_define("LspDiagnosticsSignInformation", { text = " ⁈", numhl = "LspDiagnosticsDefaultInformation" })
+-- vim.fn.sign_define("LspDiagnosticsSignHint", { text = " ⯁", numhl = "LspDiagnosticsDefaultHint" })
+-- Diagnostic text colors
+-- Errors in Red
+vim.cmd [[ hi LspDiagnosticsVirtualTextError guifg=Red ctermfg=Red ]]
+-- Warnings in Yellow
+vim.cmd [[ hi LspDiagnosticsVirtualTextWarning guifg=Yellow ctermfg=Yellow ]]
+-- Info and Hints in White
+vim.cmd [[ hi LspDiagnosticsVirtualTextInformation guifg=White ctermfg=White ]]
+vim.cmd [[ hi LspDiagnosticsVirtualTextHint guifg=White ctermfg=White ]]
+-- Underline the offending code
+vim.cmd [[ hi LspDiagnosticsUnderlineError guifg=NONE ctermfg=NONE cterm=underline gui=underline ]]
+vim.cmd [[ hi LspDiagnosticsUnderlineWarning guifg=NONE ctermfg=NONE cterm=underline gui=underline ]]
+vim.cmd [[ hi LspDiagnosticsUnderlineInformation guifg=NONE ctermfg=NONE cterm=underline gui=underline ]]
+vim.cmd [[ hi LspDiagnosticsUnderlineHint guifg=NONE ctermfg=NONE cterm=underline gui=underline ]]
