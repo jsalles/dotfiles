f1ac33a6df8174b4eae4987214b2aa74f3284b33
Joao Carlos Veiga Salles <jvsalles@amazon.com>
2023-02-25 17:44:16 +0100

    make fish my default shell

--- fish/config.fish
+++ fish/config.fish
@@ -1,3 +1,5 @@
+set PATH /Users/jvsalles/.toolbox/bin $PATH
+
 alias ccpr="aws codecommit pr --profile \$(git remote get-url origin | sed 's|codecommit::[^:]*://\([^@]*\)@.*|\1|') --pr-mode=feature-branch --role-name=Developer --skip-verify-tag=\"[SKIP CI]\""
 
 bind \cf tmux-sessionizer
