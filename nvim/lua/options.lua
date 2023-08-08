de7dca556d58c138469a4fddc45584e98dee4d4d
Joao Salles <jvsalles@amazon.de>
2023-08-08 18:36:05 +0200

    Improve folding options

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -2,46 +2,43 @@ local indent = 2
 
 vim.g.mapleader = " "
 vim.g.maplocalleader = ","
-vim.opt.autowrite = true -- enable auto write
+vim.opt.autowrite = true          -- enable auto write
 vim.opt.clipboard = "unnamedplus" -- sync with system clipboard
-vim.opt.conceallevel = 3 -- Hide * markup for bold and italic
+vim.opt.conceallevel = 3          -- Hide * markup for bold and italic
 -- vim.opt.concealcursor = "n" -- Hide * markup for bold and italic
-vim.opt.confirm = true -- confirm to save changes before exiting modified buffer
-vim.opt.cursorline = true -- Enable highlighting of the current line
-vim.opt.expandtab = true -- Use spaces instead of tabs
--- vim.opt.foldexpr = "nvim_treesitter#foldexpr()" -- TreeSitter folding
--- vim.opt.foldlevel = 6
--- vim.opt.foldmethod = "expr" -- TreeSitter folding
+vim.opt.confirm = true            -- confirm to save changes before exiting modified buffer
+vim.opt.cursorline = true         -- Enable highlighting of the current line
+vim.opt.expandtab = true          -- Use spaces instead of tabs
 vim.opt.guifont = "FiraCode Nerd Font:h12"
 vim.opt.grepprg = "rg --vimgrep"
 vim.opt.grepformat = "%f:%l:%c:%m"
-vim.opt.hidden = true -- Enable modified buffers in background
-vim.opt.ignorecase = true -- Ignore case
-vim.opt.inccommand = "split" -- preview incremental substitute
-vim.opt.joinspaces = false -- No double spaces with join after a dot
-vim.opt.list = true -- Show some invisible characters (tabs...
-vim.opt.mouse = "a" -- enable mouse mode
-vim.opt.number = true -- Print line number
-vim.opt.pumblend = 10 -- Popup blend
-vim.opt.pumheight = 10 -- Maximum number of entries in a popup
+vim.opt.hidden = true         -- Enable modified buffers in background
+vim.opt.ignorecase = true     -- Ignore case
+vim.opt.inccommand = "split"  -- preview incremental substitute
+vim.opt.joinspaces = false    -- No double spaces with join after a dot
+vim.opt.list = true           -- Show some invisible characters (tabs...
+vim.opt.mouse = "a"           -- enable mouse mode
+vim.opt.number = true         -- Print line number
+vim.opt.pumblend = 10         -- Popup blend
+vim.opt.pumheight = 10        -- Maximum number of entries in a popup
 vim.opt.relativenumber = true -- Relative line numbers
-vim.opt.scrolloff = 4 -- Lines of context
-vim.opt.shiftround = true -- Round indent
-vim.opt.shiftwidth = indent -- Size of an indent
-vim.opt.showmode = false -- dont show mode since we have a statusline
-vim.opt.sidescrolloff = 8 -- Columns of context
-vim.opt.signcolumn = "yes" -- Always show the signcolumn, otherwise it would shift the text each time
-vim.opt.smartcase = true -- Don't ignore case with capitals
-vim.opt.smartindent = true -- Insert indents automatically
-vim.opt.splitbelow = true -- Put new windows below current
-vim.opt.splitright = true -- Put new windows right of current
-vim.opt.tabstop = indent -- Number of spaces tabs count for
-vim.opt.termguicolors = true -- True color support
+vim.opt.scrolloff = 4         -- Lines of context
+vim.opt.shiftround = true     -- Round indent
+vim.opt.shiftwidth = indent   -- Size of an indent
+vim.opt.showmode = false      -- dont show mode since we have a statusline
+vim.opt.sidescrolloff = 8     -- Columns of context
+vim.opt.signcolumn = "yes"    -- Always show the signcolumn, otherwise it would shift the text each time
+vim.opt.smartcase = true      -- Don't ignore case with capitals
+vim.opt.smartindent = true    -- Insert indents automatically
+vim.opt.splitbelow = true     -- Put new windows below current
+vim.opt.splitright = true     -- Put new windows right of current
+vim.opt.tabstop = indent      -- Number of spaces tabs count for
+vim.opt.termguicolors = true  -- True color support
 vim.opt.undofile = true
 vim.opt.undolevels = 10000
-vim.opt.updatetime = 200 -- save swap file and trigger CursorHold
+vim.opt.updatetime = 200               -- save swap file and trigger CursorHold
 vim.opt.wildmode = "longest:full,full" -- Command-line completion mode
-vim.opt.wrap = false -- Disable line wrap
+vim.opt.wrap = false                   -- Disable line wrap
 vim.opt.sessionoptions = { "buffers", "curdir", "tabpages", "winsize" }
 vim.o.shortmess = "IToOlxfitn"
 -- formatoptions
@@ -69,17 +66,16 @@ vim.g.loaded_matchparen = 1
 
 -- Use proper syntax highlighting in code blocks
 local fences = {
-	"lua",
-	-- "vim",
-	"json",
-	"typescript",
-	"javascript",
-	"js=javascript",
-	"ts=typescript",
-	"shell=sh",
-	"python",
-	"sh",
-	"console=sh",
+  "lua",
+  -- "vim",
+  "json",
+  "typescript",
+  "javascript",
+  "js=javascript",
+  "ts=typescript",
+  "shell=sh",
+  "python",
+  "sh",
+  "console=sh",
 }
 vim.g.markdown_fenced_languages = fences
-
