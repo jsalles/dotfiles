daff55c30118ef88777e33d95d952284614908e5
Joao Salles <jvsalles@amazon.de>
2024-01-28 12:57:27 +0100

    Add tmux-navigation

---
+++ nvim/lua/plugins/tmux-navigation.lua
@@ -0,0 +1,17 @@
+return {
+  {
+    "alexghergh/nvim-tmux-navigation",
+    keys = { "<C-h>", "<C-j>", "<C-k>", "<C-l>" },
+    config = function()
+      require("nvim-tmux-navigation").setup({
+        disable_when_zoomed = true, -- defaults to false
+        keybindings = {
+          left = "<C-h>",
+          down = "<C-j>",
+          up = "<C-k>",
+          right = "<C-l>",
+        },
+      })
+    end,
+  },
+}
