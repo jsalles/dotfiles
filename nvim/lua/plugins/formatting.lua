a8159780e845844b91e486013e49310cb97b78be
Joao Salles <jvsalles@amazon.de>
2024-01-31 13:29:20 +0100

    Add command to disable format on save

--- nvim/lua/plugins/formatting.lua
+++ nvim/lua/plugins/formatting.lua
@@ -20,11 +20,10 @@ return {
         "<leader>cf",
         mode = { "n", "v" },
         function()
-          require("conform").format();
+          require("conform").format()
         end,
-        desc = "Format"
-      }
-
+        desc = "Format",
+      },
     },
     -- init = function()
     -- -- Install the conform formatter on VeryLazy
@@ -57,7 +56,13 @@ return {
           async = false, -- not recommended to change
           quiet = false, -- not recommended to change
         },
-        format_on_save = { timeout_ms = 500, lsp_fallback = true },
+        format_on_save = function(bufnr)
+          -- Disable with a global or buffer-local variable
+          if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+            return
+          end
+          return { timeout_ms = 500, lsp_fallback = true }
+        end,
         ---@type table<string, conform.FormatterUnit[]>
         formatters_by_ft = {
           lua = { "stylua" },
@@ -84,6 +89,27 @@ return {
       }
       return opts
     end,
-    config = true,
+    -- config = true,
+    config = function(_, opts)
+      require("conform").setup(opts)
+
+      vim.api.nvim_create_user_command("FormatDisable", function(args)
+        if args.bang then
+          -- FormatDisable! will disable formatting just for this buffer
+          vim.b.disable_autoformat = true
+        else
+          vim.g.disable_autoformat = true
+        end
+      end, {
+        desc = "Disable autoformat-on-save",
+        bang = true,
+      })
+      vim.api.nvim_create_user_command("FormatEnable", function()
+        vim.b.disable_autoformat = false
+        vim.g.disable_autoformat = false
+      end, {
+        desc = "Re-enable autoformat-on-save",
+      })
+    end,
   },
 }
