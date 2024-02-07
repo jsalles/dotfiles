0ba4e511fce5b88cfa0ed5b04e493583dc171fd1
Joao Salles <jvsalles@amazon.de>
2024-02-07 14:56:34 +0100

    Fix tmux switching panes on c-space

--- nvim/lua/plugins/completion.lua
+++ nvim/lua/plugins/completion.lua
@@ -58,7 +58,8 @@ return {
           ["<C-p>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
           ["<C-b>"] = cmp.mapping.scroll_docs(-4),
           ["<C-f>"] = cmp.mapping.scroll_docs(4),
-          ["<C-Space>"] = cmp.mapping.complete(),
+          -- ["<C-Space>"] = cmp.mapping.complete(),
+          ["<C-Space>"] = cmp.mapping(cmp.mapping.complete({}), { "i", "c" }),
           ["<C-e>"] = cmp.mapping.abort(),
           ["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
           ["<S-CR>"] = cmp.mapping.confirm({
@@ -107,15 +108,11 @@ return {
     "windwp/nvim-autopairs",
     event = { "BufReadPre", "BufNewFile" },
     config = function()
-      require("nvim-autopairs").setup();
+      require("nvim-autopairs").setup()
 
-      local cmp_autopairs = require('nvim-autopairs.completion.cmp')
-      local cmp = require('cmp')
-      cmp.event:on(
-        'confirm_done',
-        cmp_autopairs.on_confirm_done()
-      )
+      local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+      local cmp = require("cmp")
+      cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
     end,
   },
-
 }
